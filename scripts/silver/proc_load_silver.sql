abdee5a933e1535fbf9ad00913ff0811bcd72dae
Boluwatife Adeogun <adeogunbolu2023@gmail.com>
2025-11-19 14:13:43 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -87,37 +87,32 @@ BEGIN
 
     RAISE NOTICE '>> Step 2: Inserting into silver.crm_prd_info';
     BEGIN
-        INSERT INTO silver.crm_prd_info (
-            prd_id, 
-            cat_id, 
-            prd_key,
-            prd_nm,
-            prd_cost,
-            prd_line, 
-            prd_start_date,
-            prd_end_dt
-        )
-        SELECT
-            prd_id,
-            REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-            SUBSTRING(prd_key FROM 7) AS prd_key,
-            prd_nm,
-            COALESCE(prd_cost, 0)::INT AS prd_cost,
-            CASE 
-                WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-                WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-                WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-                WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-                ELSE 'n/a'
-            END AS prd_line,
-            CAST(prd_start_dt AS DATE) AS prd_start_dt,
-            CAST(
-                LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day'
-                AS DATE
-            ) AS prd_end_dt
-        FROM bronze.crm_prd_info
-        WHERE SUBSTRING(prd_key FROM 7) NOT IN (
-            SELECT sls_prd_key FROM bronze.crm_sales_details
+		INSERT INTO silver.crm_prd_info (
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_date,
+    prd_end_dt
+)
+SELECT
+    prd_id,
+    REPLACE(SUBSTRING(prd_key FROM 1 FOR 5), '-', '_') AS cat_id,
+    SUBSTRING(prd_key FROM  7) AS prd_key,
+    prd_nm,
+    COALESCE(NULLIF(prd_cost, '')::INT, 0) AS prd_cost,
+    CASE 
+        WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+        WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+        WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+        WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+        ELSE 'n/a'
+    END AS prd_line,
+    prd_start_dt::DATE,
+    LEAD(prd_start_dt) OVER (PARTITION BY SUBSTRING(prd_key FROM 7) ORDER BY prd_start_dt) - INTERVAL '1 day' AS prd_end_dt
+FROM bronze.crm_prd_info;
         );
     EXCEPTION WHEN OTHERS THEN
         RAISE WARNING 'Error in Step 2: %', SQLERRM;
